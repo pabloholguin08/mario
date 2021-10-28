6169d8283c1dd58018155f24c29735dbe10910ff
pabloholguin08 <jpholguin06@gmail.com>
2021-10-28 16:59:07 -0500

    se crea minecraft

--- app/src/main/java/com/example/mario2/dibujo2.java
+++ app/src/main/java/com/example/mario2/dibujo2.java
@@ -2,13 +2,25 @@ package com.example.mario2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class dibujo2 extends AppCompatActivity {
 
+    ImageButton boton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dibujo2);
+        setContentView(R.layout.activity_main);
+        boton2=findViewById(R.id.boton2);
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dibujo2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
