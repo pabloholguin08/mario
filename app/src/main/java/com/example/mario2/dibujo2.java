3d4dae487581e85a9820caceb92bf73277a4c448
pabloholguin08 <jpholguin06@gmail.com>
2021-10-28 15:53:52 -0500

    se crea mario

---
+++ app/src/main/java/com/example/mario2/dibujo2.java
@@ -0,0 +1,14 @@
+package com.example.mario2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dibujo2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dibujo2);
+    }
+}
